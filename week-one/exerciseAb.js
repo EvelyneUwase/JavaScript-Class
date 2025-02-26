29d46ef4a46fd8fd5fba4a3eed91f27edbf993c5
Uwase Evelyne <uwaseevelyne9@gmail.com>
2025-02-26 14:38:24 +0300

    added task

--- week-one/exerciseAb.js
+++ week-one/exerciseAb.js
@@ -42,20 +42,20 @@ console.log(capitalizedChallenge);*/
 // const arr = str.split(".");
 // console.log(arr)
 
-// replacing ???
+// replacing ??? 12
 // const str = "30 Days Of JavaScript (30 Days Python)";
 // const newStr = str.replace();
 // console.log(newStr);
 
 
-// slide 5 
-const str = '30 Days Of JavaScript';
-console.log(str.charAt(15));
+    slide5 
+// const str = '30 Days Of JavaScript';
+// console.log(str.charAt(15));
 
 // no 2 code
-const character = 'J';
-const characterCode = character.charCodeAt(0)
-console.log(characterCode);
+// const character = 'J';
+// const characterCode = character.charCodeAt(0)
+// console.log(characterCode);
 // each character is assigned to a unique numerical value aka code point
 // so uppercase J is 74 in decimal notation.
 
@@ -68,7 +68,141 @@ console.log(characterCode);
 // console.log(position);
 
 // no5
-const string = "You cannot end a sentence with because because because in a conjuction";
-const position = string.firstIndexOf("because");
-console.log(position)
+// const string = "You cannot end a sentence with because because because in a conjuction";
+// const position = string.indexOf("because");
+// console.log(position)
 
+// no-6 lastIndexOf
+// const position = string.lastIndexOf("because");
+// console.log(position)
+
+// n0-7 
+// const position = string.search("because");
+// console.log(position)
+
+// n0 8 trim
+// const trimmedString = string.trim();
+// console.log(trimmedString)
+
+
+        slide6
+// n0 1 startsWith()
+// let string = "30 Days Of JavaScript";
+// const result = string.startsWith("30");
+// console.log(result)
+// // this method returns true if the string starts with a specified value and false otherwise
+
+// const result = string.endsWith("Script");
+// console.log(result)
+
+// const result = string.match(/a/g);
+// console.log(result) /a/g is a pattern to match
+
+// const results = string.repeat(2);
+// console.log(results)
+
+// // merging 
+// const array1 = ["30 Days of"];
+// const array2 = ["JavaScript"];
+// const mergedArray = array1.concat(array2);
+
+// console.log(mergedArray)
+
+// SLIDE 7
+// console.log(typeof 10 === 10);
+
+// const num = parseFloat('9.8');
+// const result = num === 10 ? num : Math.ceil(num);
+// console.log(result); 
+
+// const python = 'python';
+// const jargon = 'jargon';
+
+// const result = python.includes('on') && jargon.includes('on');
+// console.log(result); 
+
+// const string = "I hope this course is not full of jargon";
+// const result = string.includes("jargon");
+// console.log(result); // Output: true
+
+// // random number between 0-100
+// let number = Math.random();
+// console.log(number)
+// let randomNumber = number *100;
+// console.log(randomNumber)
+// let wholeNumber = Math.floor(randomNumber)
+// console.log(wholeNumber)
+
+// // btween 50 and 100
+// let number = Math.random();
+// console.log(number)
+// let randomNumber = number*50;
+// console.log(randomNumber )
+// let wholeNumber = Math.floor(randomNumber)
+//  console.log(wholeNumber)
+
+//  let number = Math.random();
+console.log(number)
+let randomNumber = number*255;
+console.log(randomNumber )
+let wholeNumber = Math.floor(randomNumber)
+ console.log(wholeNumber)
+
+
+//  const str = 'JavaScript';
+// const randomIndex = Math.floor(Math.random() * str.length);
+// const randomChar = str[randomIndex];
+// console.log(randomChar);
+
+// // slide 8
+console.log(`1 1 1 1 1`)
+console.log(`2 1 2 4 8`)
+console.log(`3 1 3 9 27`)
+console.log(`4 1 4 16 64`)
+console.log(`5 1 5 25 125`)
+
+// slice
+const sentence = "You cannot end a sentence with because because because is a conjunction";
+const startIndex = sentence.indexOf('because because because');
+const phrase = sentence.subsentence(startIndex, 20);
+console.log(phrase); 
+// still failling
+
+// SLIDE 9
+let firstName = "Evelyne";
+let lastName = "Uwase";
+let country = "Uganda";
+let city = "Kampala";
+let age = 23;
+let isMarried = false;
+let year = 2025;
+
+console.log(typeof(firstName))
+console.log(typeof(lastName ))
+console.log(typeof(country))
+console.log(typeof(city))
+console.log(typeof(age ))
+console.log(typeof(isMarried ))
+console.log(typeof(year))
+
+// console.log(typeof 10 ===10);
+
+// check parselnt
+const num = parseInt('9,8');
+const result = Math.round(num);
+console.log(result)
+// not equal to 10
+
+// boolean 
+//  const value0 = 5 > 3; // true
+
+// const value1 = "Hello"; // "Hello" is a truthy string
+
+const value2 = [1, 2, 3];
+console.log(value2)
+
+const value4 = 0; // 0 is a falsy number
+
+ const value5 = ""; // Empty strings are falsy
+
+ const value6 = null;
---
+++ week-one/fridayTask.js
@@ -0,0 +1,39 @@
+// Exercise 2
+// from Qn.1 to Qn.6
+const now = new Date();
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+
+// Qn.7
+
+const secondsElapsed = Math.floor(Date.now() / 1000);
+console.log(secondsElapsed)
+
+// SLIDE2
+// prompt
+
+// prompt("Enter base and height")
+
+/*let b = 20, h = 10;
+let areaOfATriangle = 0.5 * b * h;
+
+console.log(areaOfATriangle)*/
+
+// The area of Triangle is 100;
+
+// no 1 . 1
+
+
+// prompt("Enter side a, side b, side c")
+
+let a = 5, b = 4, c = 3;
+let perimeterOfTriangle = a + b + c;
+console.log(perimeterOfTriangle)
+
+//The perimeter of triangle is 12
+
+
